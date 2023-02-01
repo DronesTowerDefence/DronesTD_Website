f57c090b0cbb716b2396fc0c5abf76b71d7f5003
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-01 10:34:04 +0100

    revert .

--- down/profile.php
+++ down/profile.php
@@ -99,28 +99,6 @@ Email: " . $_SESSION["email"] . "
     <br>
     <br>
 
-    <head> 
-        <script>
-            function setup() {
-                document.getElementById('buttonid').addEventListener('click', openDialog);
-                function openDialog() {
-                    document.getElementById('fileid').click();
-                }
-                document.getElementById('fileid').addEventListener('change', submitForm);
-                function submitForm() {
-                    document.getElementById('formid').submit();
-                }
-            }
-        </script> 
-    </head>
-    <body onload="setup()">
-        <form id='formid' action="form.php" method="POST" enctype="multipart/form-data"> 
-            <input id='fileid' type='file' name='filename' hidden/>
-            <input id='buttonid' type='button' value='Upload MB' /> 
-            <input type='submit' value='Submit' /> 
-        </form> 
-    </body> 
-
 
 
 
