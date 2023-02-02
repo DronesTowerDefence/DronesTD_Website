17335d22656729891dad9087524b0a92ce8c762a
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-02 12:35:38 +0100

    ""

--- down/profile.php
+++ down/profile.php
@@ -58,8 +58,8 @@
     }
 
 ?>
-<img src="../img/noPFP.png" alt="No Profile Picture" style="float:right;width:250px;height:250px;">
-<h1> Profil </h1> 
+
+<h1> Profil <img src="../img/noPFP.png" alt="No Profile Picture" style="float:right;width:250px;height:250px;"></h1> 
 
 
 <?php
