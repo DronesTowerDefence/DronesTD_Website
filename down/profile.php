c461762feeb74349a0c87aa2e3c135eaf94a8799
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-10 11:53:09 +0100

    " vergessen

--- down/profile.php
+++ down/profile.php
@@ -59,7 +59,7 @@
 
 
 
-echo "<h1> Profil <img src="/uploads/" . $_SESSION["username"] . ".* alt="Profile Picture" style="float:right;width:250px;height:250px;"></h1> ";
+echo "<h1> Profil <img src="/uploads/"" . $_SESSION["username"] . ".* alt="Profile Picture" style="float:right;width:250px;height:250px;"></h1> ";
 
 
 
