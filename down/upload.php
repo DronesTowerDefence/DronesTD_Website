b92f9c4031efae3d88efa01ebcdd55934a8a6879
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-10 11:36:28 +0100

    fix?

--- down/upload.php
+++ down/upload.php
@@ -1,11 +1,13 @@
 
 <?php
 session_start();
-//$temp1 = $_SESSION["username"];
-//mkdir("uploads/". $temp1 ."", 0700);
-
+/*$temp1 = $_SESSION["username"];
+mkdir("uploads/". $temp1 ."", 0700);*/
+$temp = explode(".", $_FILES["file"]["name"]);
+$newfilename = $_SESSION["username"]  . '.' . end($temp);
+//
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . $newfilename;
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -34,16 +36,15 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
   echo "Nur JPG, JPEG, PNG & GIF files sind erlaubt.";
   $uploadOk = 0;
 }
-$temp = explode(".", $_FILES["file"]["name"]);
-$newfilename = $_SESSION["username"]  . '.' . end($temp);
+
 // Falls obigen Abfragen null sind.
 if ($uploadOk == 0) {
-  echo " Die Datei wurde nicht hochgeladen, Bed. nicht erfüllt
+  echo " Die Datei wurde nicht hochgeladen. 
   <br><br> Hier gehts zum Profil zurück: 
   <a href='https://www.dronestd.de/down/profile.php'>-><b>Profil</b></a></p>";
 // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["file"]["tmp_name"], $newfilename)) {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "Die Datei ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " wurde hochgeladen. <br><br> Hier gehts zum Profil zurück: 
     <a href='https://www.dronestd.de/down/profile.php'>-><b>Profil</b></a></p>"; //Special Char wegen Endungen und eventuellen Sonerzeichen
   } else {
