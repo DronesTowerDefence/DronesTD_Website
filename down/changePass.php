939ac1eab76750b0533f5f72b6fbca06a0dc9d55
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-12 01:41:39 +0100

    SMTP auskommentiert

--- down/changePass.php
+++ down/changePass.php
@@ -16,14 +16,14 @@ require 'PHPMailer/src/SMTP.php';
 // create object of PHPMailer class with boolean parameter which sets/unsets exception.
 $mail = new PHPMailer(true);                              
 try {
-    $mail->isSMTP(); // using SMTP protocol                                     
+   /* $mail->isSMTP(); // using SMTP protocol                                     
     $mail->Host = 'smtp.gmail.com'; // SMTP host as gmail 
     $mail->SMTPAuth = true;  // enable smtp authentication                             
     $mail->Username = 'sender@gmail.com';  // sender gmail host              
     $mail->Password = 'password'; // sender gmail host password                          
     $mail->SMTPSecure = 'tls';  // for encrypted connection                           
     $mail->Port = 587;   // port for SMTP     
-
+*/
     $mail->setFrom('dronestd176065@web10925.cweb05.gamingcontrol.de', "Drones"); // sender's email and name
     $mail->addAddress('scheunerttim@gmail.com', "Tim");  // receiver's email and name
 
