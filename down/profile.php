5692a2d8df40b2d76fbb8f0e058708d3a0da8eb0
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-13 10:26:37 +0100

    Cache bei Profilbild deaktviert

--- down/profile.php
+++ down/profile.php
@@ -40,6 +40,10 @@
 
     session_start();
 
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
     /*
     $encryption = $_COOKIE["user"];
     $decryption_iv = '1234567891011121';
