30af9e08acfaaa5a4ce91e34cf01b947e27ef41a
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-15 10:56:40 +0100

    ""

--- down/profile.php
+++ down/profile.php
@@ -141,7 +141,7 @@ Email: <code>" . $_SESSION["email"] . "</code>
       <br>
       <label for="new_name">Neues Passwort:</label>
     <input type="password" id="new_name" name="new_name" required maxlength="20" placeholder="Mustermann2">
-    &emsp;
+    
       <button><p style="color: red">Ändern</button>
   </form>
 
