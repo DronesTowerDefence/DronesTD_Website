4ab564eec3597075bd8728a93b901465e6c1ede6
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-23 10:24:27 +0100

    ""

--- down/leaderboard.php
+++ down/leaderboard.php
@@ -85,46 +85,20 @@ echo $returnS;
 	
 */
 
-echo "<table>
+$returnS .= "<table>
 <tr>
-  <td>&nbsp;</td>
-  <td>Knocky</td>
-  <td>Flor</td>
-  <td>Ella</td>
-  <td>Juan</td>
+  <td>Username</td>
+  <td>XP</td>
 </tr>
+
 <tr>
-  <td>Breed</td>
-  <td>Jack Russell</td>
-  <td>Poodle</td>
-  <td>Streetdog</td>
-  <td>Cocker Spaniel</td>
-</tr>
-<tr>
-  <td>Age</td>
-  <td>16</td>
-  <td>9</td>
-  <td>10</td>
-  <td>5</td>
-</tr>
-<tr>
-  <td>Owner</td>
-  <td>Mother-in-law</td>
-  <td>Me</td>
-  <td>Me</td>
-  <td>Sister-in-law</td>
-</tr>
-<tr>
-  <td>Eating Habits</td>
-  <td>Eats everyone's leftovers</td>
-  <td>Nibbles at food</td>
-  <td>Hearty eater</td>
-  <td>Will eat till he explodes</td>
+  <td>Account</td>
+  <td>0</td>
 </tr>
 </table>
 ";
 
-
+echo $returnS;
 
 ?>
 
