4a2c7b37dd6e0a3781d1c7cae2d55a2385981ad7
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-02-23 10:21:45 +0100

    ""

--- down/leaderboard.php
+++ down/leaderboard.php
@@ -67,7 +67,7 @@ $result = $conn->query($sqlCheck);
 
 $returnS = "<table><tr><th>Name</th><th>XP</th></tr><tr>";
 	
-	
+/*
 for($i = 0; $i < $result->num_rows; $i++){
 	
 	$row = $result->fetch_assoc();
@@ -83,11 +83,51 @@ $returnS .= "</tr></table>";
 	
 echo $returnS;
 	
+*/
 
+echo "<table>
+<tr>
+  <td>&nbsp;</td>
+  <td>Knocky</td>
+  <td>Flor</td>
+  <td>Ella</td>
+  <td>Juan</td>
+</tr>
+<tr>
+  <td>Breed</td>
+  <td>Jack Russell</td>
+  <td>Poodle</td>
+  <td>Streetdog</td>
+  <td>Cocker Spaniel</td>
+</tr>
+<tr>
+  <td>Age</td>
+  <td>16</td>
+  <td>9</td>
+  <td>10</td>
+  <td>5</td>
+</tr>
+<tr>
+  <td>Owner</td>
+  <td>Mother-in-law</td>
+  <td>Me</td>
+  <td>Me</td>
+  <td>Sister-in-law</td>
+</tr>
+<tr>
+  <td>Eating Habits</td>
+  <td>Eats everyone's leftovers</td>
+  <td>Nibbles at food</td>
+  <td>Hearty eater</td>
+  <td>Will eat till he explodes</td>
+</tr>
+</table>
+";
 
 
 
 ?>
+
 
 </body>
 </html>
