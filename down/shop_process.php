228de84d17eaae5b131032d5e4fc1b9385fcb788
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-03-06 20:59:41 +0100

    ""

--- down/shop_process.php
+++ down/shop_process.php
@@ -3,3 +3,6 @@
 //Es gibt grundsätzlich 2  Arten, GET UND POST. Bei GET wird aber in der URL DAten mitgegeben, und das schickt sich bei Passwörtern nicht.
 $username = $_POST["username"];
 $amount = $_POST["amount"];
+
+
+echo $username . $amount;
