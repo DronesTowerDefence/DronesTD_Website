c656cf38c94f78a77245ceba1a8977803e4764d6
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-03-13 00:33:25 +0100

    ""

--- down/signin_process.php
+++ down/signin_process.php
@@ -40,46 +40,38 @@ if ($result->num_rows == 0) {
     <a href='https://www.dronestd.de/down/sign-in.php'>-><b>Anmeldung</b></a></p>");
 
 }
-$row = $result->fetch_assoc();
-
-if(password_verify($passwort, $row["passwort"])){
-
-    die("Passwort stimmt nicht überein!
-    <br><br> Hier gehts zurück: 
-    <a href='https://www.dronestd.de/down/sign-in.php'>-><b>Anmeldung</b></a></p>");
-
-}
-
-//CSS hierhin:
-
-//::
-
 //fetch_assoc übergibt die erste Spalte.(in einer Forshcleife geht es automatisch zur nächsten Spalte bei jedem Aufruf,
 //aber da wir nur einen User mit den Daten haben, gibt es nur einen Aufruf)
 //Nun können wir mit nennen des Attributnamen der DB den Inhalt in $row zwischenspeichern
+$row = $result->fetch_assoc();
 
+if(password_verify($passwort, $row["passwort"])){
+    $username = $row["username"];
+    $admin = $row["admin"];
+    
+    //Sessions werden gesetzt für einfache Verwendung in Zukunft, (damit nicht auf jeder Seite eine sql-Abfrage gemacht werden muss)
+    
+    $_SESSION["email"] = $email;
+    $_SESSION["username"] = $username;
+    $_SESSION["loggedin"] = 1; //eingeloggt-Status
+    $_SESSION["admin"] = $admin;
+    
+    //JavaScript weiterleitung
+    echo "
+    <script type=\"text/javascript\">
+    
+    window.open('profile.php', '_self'); 
+    
+    </script>
+    ";
+    
+    //Shcließen der Datenbbank
+    $conn->close();
+}else{
+    die("Passwort stimmt nicht überein!
+    <br><br> Hier gehts zurück: 
+    <a href='https://www.dronestd.de/down/sign-in.php'>-><b>Anmeldung</b></a></p>");
+}
 
-$username = $row["username"];
-$admin = $row["admin"];
-
-//Sessions werden gesetzt für einfache Verwendung in Zukunft, (damit nicht auf jeder Seite eine sql-Abfrage gemacht werden muss)
-
-$_SESSION["email"] = $email;
-$_SESSION["username"] = $username;
-$_SESSION["loggedin"] = 1; //eingeloggt-Status
-$_SESSION["admin"] = $admin;
-
-//JavaScript weiterleitung
-echo "
-<script type=\"text/javascript\">
-
-window.open('profile.php', '_self'); 
-
-</script>
-";
-
-//Shcließen der Datenbbank
-$conn->close();
 
 ?>
-<!-- #endregion -->
