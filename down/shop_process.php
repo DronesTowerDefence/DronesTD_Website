c32b23896e4a5f53162d507c6e6613f83d6de420
Tim Scheunert <tim.scheunert@schueler.max-eyth-schule.de>
2023-03-12 18:23:22 +0100

    shop_processs angefangen

--- down/shop_process.php
+++ down/shop_process.php
@@ -1,8 +1,8 @@
 <?php
 //Hier speicher ich die mit dem POST übergebenen Elemente in Variablen.
 //Es gibt grundsätzlich 2  Arten, GET UND POST. Bei GET wird aber in der URL DAten mitgegeben, und das schickt sich bei Passwörtern nicht.
-$username = $_POST["username"];
-$amount = $_POST["amount"];
+$username = $_GET["username"];
+$amount = $_GET["amount"];
 
 
 echo $username . $amount;
